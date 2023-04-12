7dc90716999c009a22599abcea7676bf53f851c6
Rahul <joinmerahul5714@gmail.com>
2023-04-13 00:53:31 +0530

    decoder's commit

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,77 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_flutter/models/post.dart';
+import 'package:instagram_flutter/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<String> uploadPost(String description, Uint8List file, String uid,
+      String username, String profImage) async {
+    String res = "some error occurred";
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+      String postId = const Uuid().v1();
+      Post post = Post(
+          description: description,
+          uid: uid,
+          postId: postId,
+          username: username,
+          datePublished: DateTime.now(),
+          postUrl: photoUrl,
+          profImage: profImage,
+          likes: []);
+      _firestore.collection('post').doc(postId).set(post.toJson());
+      res = "success";
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+
+  Future<void> likePost(String postId, String uid, List likes) async {
+    try {
+      if (likes.contains(uid)) {
+        await _firestore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayRemove([uid])
+        });
+      } else {
+        await _firestore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayRemove([uid])
+        });
+      }
+    } catch (e) {}
+  }
+
+  Future<void> postcomment(String postId, String text, String uid, String name,
+      String profilPic) async {
+    try {
+      if (text.isNotEmpty) {
+        String commentId = const Uuid().v1();
+        await _firestore
+            .collection('posts')
+            .doc(postId)
+            .collection('comments')
+            .doc(commentId)
+            .set({
+          'profilePic': profilPic,
+          'name': name,
+          'uid': uid,
+          'text': text,
+          'commentId': commentId,
+          'datePublished': DateTime.now()
+        });
+      } else {
+        print('Text is empty');
+      }
+    } catch (e) {}
+  }
+
+  Future<void> deletePost(String postId) async {
+    try {
+      await _firestore.collection('posts').doc(postId).delete();
+    } catch (err) {}
+  }
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,196 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_flutter/resources/auth_methods.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/utils.dart';
+import 'package:instagram_flutter/widgets/text_field_input.dart';
+
+import '../responsive/mobile_screen_layout.dart';
+import '../responsive/responsive_layout_screen.dart';
+import '../responsive/web_screen_layout.dart';
+import 'login_screen.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  Uint8List? _image;
+  bool _isLoading = false;
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
+  }
+
+  Future<void> selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = im;
+    });
+  }
+
+  void signUpUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res = await AuthMethods().signUpUser(
+        email: _emailController.text,
+        password: _passwordController.text,
+        username: _usernameController.text,
+        bio: _bioController.text,
+        file: _image!);
+    setState(() {
+      _isLoading = false;
+    });
+    if (res != 'success') {
+      showSnackBar(res, context);
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const ResponsiveLayout(
+            mobileScreenLayout: MobileScreenLayout(),
+            webScreenLayout: WebScreenLayout(),
+          ),
+        ),
+      );
+    }
+  }
+
+  void navigateToLogin() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const LoginScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Flexible(
+            child: Container(),
+            flex: 2,
+          ),
+          // ignore: deprecated_member_use
+          SvgPicture.asset(
+            'assets/ic_instagram.svg',
+            height: 64,
+          ),
+          const SizedBox(
+            height: 64,
+          ),
+          Stack(
+            children: [
+              _image != null
+                  ? CircleAvatar(
+                      radius: 64, backgroundImage: MemoryImage(_image!))
+                  : const CircleAvatar(
+                      radius: 64,
+                      backgroundImage: NetworkImage(
+                          'https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg'),
+                    ),
+              Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                    onPressed: selectImage,
+                    icon: Icon(Icons.add_a_photo),
+                  ))
+            ],
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+              textEditingController: _usernameController,
+              hintText: 'Enter your username',
+              textInputType: TextInputType.text),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+              textEditingController: _emailController,
+              hintText: 'Enter your email',
+              textInputType: TextInputType.emailAddress),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+            textEditingController: _passwordController,
+            hintText: 'Enter your password',
+            textInputType: TextInputType.text,
+            ispass: true,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+              textEditingController: _bioController,
+              hintText: 'Enter your bio',
+              textInputType: TextInputType.text),
+          const SizedBox(
+            height: 24,
+          ),
+          InkWell(
+            onTap: signUpUser,
+            child: Container(
+              child: _isLoading
+                  ? Center(
+                      child: CircularProgressIndicator(
+                        color: primaryColor,
+                      ),
+                    )
+                  : const Text('Sign up'),
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  color: blueColor),
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Flexible(child: Container(), flex: 2),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: const Text("Already have an account ? "),
+                padding: const EdgeInsets.symmetric(vertical: 8),
+              ),
+              GestureDetector(
+                onTap: navigateToLogin,
+                child: Container(
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+              )
+            ],
+          )
+        ]),
+      )),
+    );
+  }
+}
